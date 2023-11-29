55a1495cef53dce272bff7ab76df0eabde22b9d1
Andreas Alstad <andralst@ulrik.uio.no>
2023-11-29 16:00:19 +0100

    view image through layers WIP

--- CNN_with_XGBoost.py
+++ CNN_with_XGBoost.py
@@ -1,10 +1,13 @@
 import numpy as np
+from collections import defaultdict
 import matplotlib.pyplot as plt
 from tensorflow import keras as tfk
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import xgboost as xgb
 from xgboost import XGBClassifier
+from sklearn.utils import shuffle
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.preprocessing import OneHotEncoder
 import cv2      # Only for simple rescaling of images
@@ -28,14 +31,6 @@ def xgboost_model(x_train, t_train, x_test, t_test, max_depth=10, eta=0.3, num_c
 
     # Train the model
     model = xgb.train(params, d_train, n_boosts)
-    # model = XGBClassifier(max_leaves=max_depth, 
-    #                       learning_rate=eta, 
-    #                       objective="multi:softprob", 
-    #                       num_class=num_class, 
-    #                       eval_metric="mlogloss", 
-    #                       reg_lambda=l2_lambda, 
-    #                       random_state=seed)
-    # model.fit(x_train, t_train, eval_set=[(x_test, t_test)], verbose=False)
 
     return model
 
@@ -88,7 +83,55 @@ def tensorflow_model(x_train, t_train, x_test, t_test, batch_size=64, epochs=10,
 
     return model, history
 
-def initialize_data():
+def augment_images(image, augmentation_datagen, aug_count) -> list:
+    augmented_images = []
+    img = np.expand_dims(image, 0)  # Add batch dimension
+    img_gen = augmentation_datagen.flow(img, batch_size=1)
+    for _ in range(aug_count):
+        augmented_images.append(img_gen.next()[0])  # Get the augmented image
+    return augmented_images
+
+def balance_classes(images, labels, num_samples, augmentation_datagen) -> tuple:
+    images_by_class = defaultdict(list)
+    labels_by_class = defaultdict(list)
+
+    # Separate images by class
+    for i, label in enumerate(np.argmax(labels, axis=1)):
+        images_by_class[label].append(images[i])
+        labels_by_class[label].append(labels[i])
+
+    balanced_images = []
+    balanced_labels = []
+    for label, imgs in images_by_class.items():
+        current_count = len(imgs)
+        if current_count < num_samples:
+            # Calculate how many augmented images are needed
+            aug_count = num_samples - current_count
+
+            # Augment images
+            for img in imgs:
+                augmented_images = augment_images(img, augmentation_datagen, aug_count // current_count)
+                balanced_images.extend(augmented_images)
+
+            # Extend the original images and labels
+            balanced_images.extend(imgs)
+            balanced_labels.extend(labels_by_class[label] * (1 + aug_count // current_count))
+
+            # Add additional augmented images if necessary
+            additional_count = num_samples - len(balanced_images)
+            if additional_count > 0:
+                extra_augmented_images = augment_images(imgs[0], augmentation_datagen, additional_count)
+                balanced_images.extend(extra_augmented_images)
+                balanced_labels.extend([labels_by_class[label][0]] * additional_count)
+        else:
+            # Randomly select images if there are too many
+            indices = np.random.choice(range(current_count), num_samples, replace=False)
+            balanced_images.extend(np.array(imgs)[indices])
+            balanced_labels.extend(np.array(labels_by_class[label])[indices])
+
+    return np.array(balanced_images), np.array(balanced_labels)
+
+def initialize_data() -> tuple:
     # Load the dataset from the dataset folder
     train_dataset = tfk.preprocessing.image_dataset_from_directory(
         "dataset/train",
@@ -112,10 +155,17 @@ def initialize_data():
     train_dataset = train_dataset.map(lambda x, y: (preprocessing_model(x), y))
     test_dataset = test_dataset.map(lambda x, y: (preprocessing_model(x), y))
 
-    # Take only some images from the dataset
-    train_dataset = train_dataset.take(1000)
-    test_dataset = test_dataset.take(200)
-
+    # Augmentation setup
+    augmentation_datagen = ImageDataGenerator(
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest'
+    )
+    
     # Split the dataset into images and labels as numpy arrays
     x_train_list = []
     t_train_list = []
@@ -133,6 +183,14 @@ def initialize_data():
     x_test = np.concatenate(x_test_list, axis=0)
     t_test = np.concatenate(t_test_list, axis=0)
 
+    # Balance the classes
+    x_train, t_train = balance_classes(x_train, t_train, target_num_images, augmentation_datagen)
+    x_test, t_test = balance_classes(x_test, t_test, target_num_images, augmentation_datagen)
+
+    # Shuffle the data
+    x_train, t_train = shuffle(x_train, t_train, random_state=seed)
+    x_test, t_test = shuffle(x_test, t_test, random_state=seed)
+
     return x_train, t_train, x_test, t_test
 
 def view_image_through_cnn_layers(model, image):
@@ -143,43 +201,43 @@ def view_image_through_cnn_layers(model, image):
     layer_outputs = [layer.output for layer in model.layers[1:4]]
     activation_model = tfk.models.Model(inputs=model.input, outputs=layer_outputs)
 
-    print([layer.name for layer in activation_model.layers])
+    print(layer_names[0:-1])
 
-    # # Get the activations for the image
-    # activations = activation_model.predict(image)
+    # Get the activations for the image
+    activations = activation_model.predict(image)
 
-    # # Visualize the activations
-    # images_per_row = 16
-    # for layer_name, layer_activation in zip(layer_names, activations):
-    #     # Number of features in the feature map
-    #     n_features = layer_activation.shape[-1]
+    # Visualize the activations
+    images_per_row = 16
+    for layer_name, layer_activation in zip(layer_names, activations):
+        # Number of features in the feature map
+        n_features = layer_activation.shape[-1]
 
-    #     # The feature map has shape (1, size, size, n_features)
-    #     size = layer_activation.shape[1]
+        # The feature map has shape (1, size, size, n_features)
+        size = layer_activation.shape[1]
 
-    #     # Tiles the activation channels in this matrix
-    #     n_cols = n_features // images_per_row
-    #     display_grid = np.zeros((size * n_cols, images_per_row * size))
+        # Tiles the activation channels in this matrix
+        n_cols = n_features // images_per_row
+        display_grid = np.zeros((size * n_cols, images_per_row * size))
 
-    #     # Tiles each filter into a big horizontal grid
-    #     for col in range(n_cols):
-    #         for row in range(images_per_row):
-    #             channel_image = layer_activation[0, :, :, col * images_per_row + row]
-    #             # Post-process the feature to make it visually palatable
-    #             channel_image -= channel_image.mean()
-    #             channel_image /= channel_image.std()
-    #             channel_image *= 64
-    #             channel_image += 128
-    #             channel_image = np.clip(channel_image, 0, 255).astype("uint8")
-    #             display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
+        # Tiles each filter into a big horizontal grid
+        for col in range(n_cols):
+            for row in range(images_per_row):
+                channel_image = layer_activation[0, :, :, col * images_per_row + row]
+                # Post-process the feature to make it visually palatable
+                channel_image -= channel_image.mean()
+                channel_image /= channel_image.std()
+                channel_image *= 64
+                channel_image += 128
+                channel_image = np.clip(channel_image, 0, 255).astype("uint8")
+                display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
 
-    #     # Display the grid
-    #     scale = 1. / size
-    #     plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
-    #     plt.title(layer_name)
-    #     plt.grid(False)
-    #     plt.imshow(display_grid, aspect="auto", cmap="viridis")
-    # plt.show()
+        # Display the grid
+        scale = 1. / size
+        plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect="auto", cmap="viridis")
+    plt.show()
 
 
 def downscale():
@@ -192,6 +250,23 @@ def downscale():
 
     return x_train_downscaled, x_test_downscaled
 
+def plot_dataset_balance(labels):
+    # Assuming t_train_balanced is your balanced label array and is one-hot encoded
+    class_indices = np.argmax(labels, axis=1)
+
+    # Count the occurrences of each class index
+    class_counts = np.bincount(class_indices)
+    categories = np.arange(len(class_counts))  # This will be your x-axis (the number of the category)
+
+    # Plot the number of images per category
+    plt.figure(figsize=(10, 6))
+    plt.bar(categories, class_counts, color='skyblue')
+    plt.xlabel('Category Number')
+    plt.ylabel('Number of Images')
+    plt.title('Number of Images per Category After Resampling')
+    plt.xticks(categories)
+    plt.show()
+
 def main():
     # One-hot encode the labels
     encoder = OneHotEncoder()
@@ -201,6 +276,9 @@ def main():
     # Load the CNN model and predict
     tf_model, tf_history = tensorflow_model(x_train, t_train, x_test, t_test, eta = learning_rate, batch_size=batch_size, epochs=n_epochs, save_results=False)
     cnn_predict = tf_model.predict(x_test)
+
+    # View an image through the CNN layers
+    view_image_through_cnn_layers(tf_model, x_test[0])
 
     # Initialize the xgboost model
     xgboost_layer_model = tfk.models.Model(inputs=tf_model.input, 
@@ -218,6 +296,7 @@ def main():
                                    x_test_downscaled, 
                                    np.argmax(t_test, axis=1))
     xgb_predict_pure = xgb_model_pure.predict(xgb.DMatrix(x_test_downscaled))
+
 
     # Scores
     cnn_score = accuracy_score(np.argmax(t_test, axis=1), np.argmax(cnn_predict, axis=1))
@@ -252,14 +331,19 @@ if __name__ == "__main__":
     seed = 42
     learning_rate = 0.001
     l2_lambda = 0.001
-    n_epochs = 50
+    n_epochs = 5
     batch_size = 32
+    target_num_images = 1000  # Set your target number of images per category
+
+
     labels = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
     n_classes = len(labels)
 
     # Initialize the data
     x_train, t_train, x_test, t_test = initialize_data()
-    x_train_downscaled, x_test_downscaled = downscale()
+    x_train_downscaled, x_test_downscaled = downscale()     # For XGBoost without CNN
 
     # Run
-    main()
+    print(t_train.shape)
+    plot_dataset_balance(t_train)
+    # main()
