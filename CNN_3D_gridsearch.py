4bafacd96058129ac099c63f60ccf6aff58946c3
andralst <andralst@uio.no>
2023-12-12 10:38:54 +0100

    final grid search script, added doc strings

--- CNN_3D_gridsearch.py
+++ CNN_3D_gridsearch.py
@@ -1,40 +1,36 @@
 import random
 import tensorflow as tf
-import zipfile
 import numpy as np
-from collections import defaultdict
 import matplotlib.pyplot as plt
-import seaborn as sns
 from tensorflow import keras as tfk
 from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Functions
-def xgboost_model(x_train, t_train, x_test, t_test, max_depth=10, eta=0.3, num_class=7, n_boosts=20):
-    # Convert the data to DMatrix format
-    d_train = xgb.DMatrix(x_train, label=t_train)
-    d_test = xgb.DMatrix(x_test, label=t_test)
+def tensorflow_model(x_train, t_train, x_test, t_test, batch_size=64, epochs=10, eta = 0.0001, l2_lambda = 0.0001, save_results = None):
+    """
+    Trains a CNN model using TensorFlow.
+    Builds the model with an architecture of 3 convolutional layers, 3 max pooling layers, 1 dropout layer, 1 flatten layer, and 3 dense layers.
+    The model is compiled with the Adam optimizer, categorical crossentropy loss function, and accuracy metric.
+    Then, the model is trained with the given training data and validated with the given testing data.
+    If save_results is not None, the model is saved as a .h5 file with the given file name given by save_results as a string.
+    Lastly, the trained model and training history are returned.
 
-    # Set the parameters for the xgboost model
-    params = {
-        "seed": seed,
-        "max_depth": max_depth,
-        "eta": eta,
-        "objective": "multi:softprob",
-        "num_class": num_class,
-        "eval_metric": "mlogloss",
-        "lambda": l2_lambda
-    }
+    Parameters:
+    - x_train (numpy.ndarray): Training data input.
+    - t_train (numpy.ndarray): Training data target.
+    - x_test (numpy.ndarray): Testing data input.
+    - t_test (numpy.ndarray): Testing data target.
+    - batch_size (int): Number of samples per gradient update. Default is 64.
+    - epochs (int): Number of epochs to train the model. Default is 10.
+    - eta (float): Learning rate for the optimizer. Default is 0.0001.
+    - l2_lambda (float): L2 regularization lambda value. Default is 0.0001.
+    - save_results (str): File name to save the trained model. Default is None.
 
-    # Train the model
-    model = xgb.train(params, d_train, n_boosts)
-
-    return model
-
-def tensorflow_model(x_train, t_train, x_test, t_test, batch_size=64, epochs=10, eta = 0.0001, l2_lambda = 0.0001, save_results = False):
-    # Set the seed
-    #tfk.utils.set_random_seed(seed)
-
+    Returns:
+    - model (tensorflow.python.keras.engine.sequential.Sequential): Trained CNN model.
+    - history (tensorflow.python.keras.callbacks.History): Training history containing score per epoch.
+    """
+    
     # Initialize CNN model
     model = tfk.Sequential([
         tfk.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(48, 48, 1), name="conv2d_1"),
@@ -74,61 +70,30 @@ def tensorflow_model(x_train, t_train, x_test, t_test, batch_size=64, epochs=10,
         callbacks=[checkpoint_callback]  # Put the callback inside a list
     )
 
-    if save_results:
+    if save_results is not None:
         # Save the model
-        model.save("model.h5")
+        model.save(f"{save_results}.h5")
 
     return model, history
 
-def augment_images(image, augmentation_datagen, aug_count) -> list:
-    augmented_images = []
-    img = np.expand_dims(image, 0)  # Add batch dimension
-    img_gen = augmentation_datagen.flow(img, batch_size=1)
-    for _ in range(aug_count):
-        augmented_images.append(img_gen.next()[0])  # Get the augmented image
-    return augmented_images
-
-def balance_classes(images, labels, num_samples, augmentation_datagen) -> tuple:
-    images_by_class = defaultdict(list)
-    labels_by_class = defaultdict(list)
-
-    # Separate images by class
-    for i, label in enumerate(np.argmax(labels, axis=1)):
-        images_by_class[label].append(images[i])
-        labels_by_class[label].append(labels[i])
-
-    balanced_images = []
-    balanced_labels = []
-    for label, imgs in images_by_class.items():
-        current_count = len(imgs)
-        if current_count < num_samples:
-            # Calculate how many augmented images are needed
-            aug_count = num_samples - current_count
-
-            # Augment images
-            for img in imgs:
-                augmented_images = augment_images(img, augmentation_datagen, aug_count // current_count)
-                balanced_images.extend(augmented_images)
-
-            # Extend the original images and labels
-            balanced_images.extend(imgs)
-            balanced_labels.extend(labels_by_class[label] * (1 + aug_count // current_count))
-
-            # Add additional augmented images if necessary
-            additional_count = num_samples - len(balanced_images)
-            if additional_count > 0:
-                extra_augmented_images = augment_images(imgs[0], augmentation_datagen, additional_count)
-                balanced_images.extend(extra_augmented_images)
-                balanced_labels.extend([labels_by_class[label][0]] * additional_count)
-        else:
-            # Randomly select images if there are too many
-            indices = np.random.choice(range(current_count), num_samples, replace=False)
-            balanced_images.extend(np.array(imgs)[indices])
-            balanced_labels.extend(np.array(labels_by_class[label])[indices])
-
-    return np.array(balanced_images), np.array(balanced_labels)
-
 def initialize_data(batch_size) -> tuple:
+    """
+    Initialize the data by loading and preprocessing the dataset as training and test data separately in gray scale and categorical labels.
+    The data is then rescaled to values between 0 and 1. Lastly, the data is split into images and labels as numpy arrays.
+    The images and labels of the training and testing data are then returned as four numpy arrays in a tuple in the format (x_train, t_train, x_test, t_test).
+
+    Args:
+        batch_size (int): The batch size for training and testing.
+
+    Returns:
+        tuple: A tuple containing the training and testing data as numpy arrays.
+            The tuple is in the format (x_train, t_train, x_test, t_test).
+            - x_train (numpy.ndarray): The training images.
+            - t_train (numpy.ndarray): The training labels.
+            - x_test (numpy.ndarray): The testing images.
+            - t_test (numpy.ndarray): The testing labels.
+    """
+
     # Load the dataset from the dataset folder
     train_dataset = tfk.preprocessing.image_dataset_from_directory(
         "dataset/train",
@@ -151,17 +116,6 @@ def initialize_data(batch_size) -> tuple:
     ])
     train_dataset = train_dataset.map(lambda x, y: (preprocessing_model(x), y))
     test_dataset = test_dataset.map(lambda x, y: (preprocessing_model(x), y))
-
-    # Augmentation setup
-    augmentation_datagen = ImageDataGenerator(
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest'
-    )
     
     # Split the dataset into images and labels as numpy arrays
     x_train_list = []
@@ -180,19 +134,31 @@ def initialize_data(batch_size) -> tuple:
     x_test = np.concatenate(x_test_list, axis=0)
     t_test = np.concatenate(t_test_list, axis=0)
 
-    # Balance the classes
-    # x_train, t_train = balance_classes(x_train, t_train, target_num_images, augmentation_datagen)
-    # x_test, t_test = balance_classes(x_test, t_test, target_num_images, augmentation_datagen)
-
-    # Shuffle the data
-    # x_train, t_train = shuffle(x_train, t_train, random_state=seed)
-    # x_test, t_test = shuffle(x_test, t_test, random_state=seed)
-
     return x_train, t_train, x_test, t_test
 
 
-def grid_search_3D(axes, epochs, plot_name = None, mode = 'loss'):
-    
+def grid_search_3D(axes, n_epochs):
+    """
+    Perform a 3D grid search for hyperparameter tuning in a convolutional neural network.
+    A 3D ndarray is created from the lengths of the axes, and used to store the scores. The score of each combination of hyperparameters is then calculated. 
+    The best score and their indices are updated if the next 3D grid point scored better than the last.
+    Lastly, the scores and their indices are returned.
+
+    Args:
+        axes (tuple): A tuple containing the axes for the grid search. Each axis represents a hyperparameter.
+                      The order of the axes should be (eta_arr, lmbda_arr, batch_arr).
+        n_epochs (int): The number of epochs to train the neural network for.
+
+    Returns:
+        tuple: A tuple containing the following elements:
+            - score_acc (ndarray): A 3D array containing the accuracy scores for each combination of hyperparameters.
+            - score_loss (ndarray): A 3D array containing the loss scores for each combination of hyperparameters.
+            - highest_acc (float): The highest accuracy score achieved during the grid search.
+            - lowest_loss (float): The lowest loss score achieved during the grid search.
+            - highest_acc_index (tuple): The indices of the hyperparameters that achieved the highest accuracy score.
+            - lowest_loss_index (tuple): The indices of the hyperparameters that achieved the lowest loss score.
+    """
+
     # Define dimensions
     eta_arr, lmbda_arr, batch_arr = axes
     Nx, Ny, Nz = len(eta_arr), len(lmbda_arr), len(batch_arr)
@@ -205,16 +171,15 @@ def grid_search_3D(axes, epochs, plot_name = None, mode = 'loss'):
     lowest_loss = 100
     lowest_loss_index = (0, 0, 0)
 
+    n_iter = 1
     for i, eta in enumerate(eta_arr):
         for j, lmbda in enumerate(lmbda_arr):
             for k, batch_size in enumerate(batch_arr):
-                n_iter = 1
-                tot_iter = Nx + Ny + Nz
-                print(f"\nIteration: {n_iter}/{tot_iter}")
+                print(f"\nIteration: {n_iter}/{Nx * Ny * Nz}")
                 n_iter += 1
                 
                 x_train, t_train, x_test, t_test = initialize_data(batch_size)
-                tf_model, tf_history = tensorflow_model(x_train, t_train, x_test, t_test, eta = eta, l2_lambda=lmbda, batch_size=batch_size, epochs=n_epochs, save_results=False)
+                tf_model, tf_history = tensorflow_model(x_train, t_train, x_test, t_test, eta = eta, l2_lambda=lmbda, batch_size=batch_size, epochs=n_epochs, save_results=f"CNN_eta{i}_lambda{j}_batch{k}")
                 cnn_predict = tf_model.predict(x_test)
                 cnn_accuracy = tf_history.history["val_accuracy"][-1]
                 cnn_loss = tf_history.history["val_loss"][-1]
@@ -226,6 +191,31 @@ def grid_search_3D(axes, epochs, plot_name = None, mode = 'loss'):
                 if cnn_loss < lowest_loss:
                     lowest_loss = cnn_loss
                     lowest_loss_index = (i, j, k)
+    
+    return score_acc, score_loss, highest_acc, lowest_loss, highest_acc_index, lowest_loss_index
+    
+def plot_3D_grid(score_acc, score_loss, axes, highest_acc, lowest_loss, highest_acc_index, lowest_loss_index, plot_name = None):
+    """
+    Plots a 3D grid of accuracy and loss values. 
+    The accuracy and loss values are plotted as heat maps, while the best accuracy and loss values are annotated.
+
+    Parameters:
+    - score_acc (ndarray): Array of accuracy values.
+    - score_loss (ndarray): Array of loss values.
+    - axes (tuple): Tuple of arrays representing the axes values (eta_arr, lmbda_arr, batch_arr).
+    - highest_acc (float): Highest accuracy value.
+    - lowest_loss (float): Lowest loss value.
+    - highest_acc_index (tuple): Indices of the highest accuracy value in the grid.
+    - lowest_loss_index (tuple): Indices of the lowest loss value in the grid.
+    - plot_name (str): Name of the plot (optional).
+
+    Returns:
+    - None
+    """    
+
+    # Unpack and define dimensions
+    eta_arr, lmbda_arr, batch_arr = axes
+    Nx, Ny, Nz = len(eta_arr), len(lmbda_arr), len(batch_arr)
     
     #X, Y, Z = np.meshgrid(eta_arr, lmbda_arr, batch_arr)
     X, Y, Z = np.meshgrid(np.arange(Nx), np.arange(Ny), np.arange(Nz))
@@ -290,6 +280,37 @@ def grid_search_3D(axes, epochs, plot_name = None, mode = 'loss'):
     plt.savefig(plot_name+"_loss")
     plt.close()
     
+def save_array(arrays: tuple, filename: str):
+    """
+    Save the arrays NumPy in a NumPy bytecode file (.npy).
+    A meshgrid is constructed from the arrays. The arrays are then flattened, 
+    concatenated and saved as a 2D array.
+
+    Parameters:
+    arrays (tuple): A tuple containing the ndarrays to be saved (x, y, z, values).
+    filename (str): The name of the file to save the arrays.
+
+    Returns:
+    None
+    """
+    x, y, z, values = arrays
+    X, Y, Z = np.meshgrid(x, y, z)
+    
+    print("shape before")
+    print(X.shape, Y.shape, Z.shape, values.shape)
+    
+    values = values.ravel().reshape(-1, 1)
+    X = X.ravel().reshape(-1, 1)
+    Y = Y.ravel().reshape(-1, 1)
+    Z = Z.ravel().reshape(-1, 1)
+    
+    print("shape after")
+    print(X.shape, Y.shape, Z.shape, values.shape)
+
+    XYZ = np.concatenate((X, Y, Z, values), axis=1)
+    
+    np.save(filename, XYZ)
+    
 seed = 42
 random.seed(seed)
 np.random.seed(seed)
@@ -297,10 +318,13 @@ tf.random.set_seed(seed)
 
 eta_space = np.logspace(-1, -5, 5)
 lambda_space = np.logspace(-1, -5, 5)
-batch_space = [8, 16, 32, 64, 128]
+batch_space = np.array([8, 16, 32, 64, 128])
 n_epochs = 300
 target_num_images = 1000  # Set your target number of images per category
 labels = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
 n_classes = len(labels)
 
-grid_search_3D((eta_space, lambda_space, batch_space), epochs = n_epochs, plot_name='CNN_gridsearch_eta_lambda_batch')
+score_acc, score_loss, highest_acc, lowest_loss, highest_acc_index, lowest_loss_index = grid_search_3D((eta_space, lambda_space, batch_space), epochs = n_epochs)
+#plot_3D_grid(score_acc, score_loss, (eta_space, lambda_space, batch_space), highest_acc, lowest_loss, highest_acc_index, lowest_loss_index, plot_name = 'CNN_gridsearch_eta_lambda_batch')
+save_array((eta_space, lambda_space, batch_space, score_acc), filename='CNN_score_accuracy')
+save_array((eta_space, lambda_space, batch_space, score_loss), filename='CNN_score_loss')
